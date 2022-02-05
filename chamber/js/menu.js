b10bcfdb2191e721b408a256d8899012acac5b06
Vanessa Buzzell <buz20001@byui.edu>
2022-02-05 15:36:32 -0500

    c

--- chamber/js/menu.js
+++ chamber/js/menu.js
@@ -1,7 +1,22 @@
+
 const menu = document.querySelector("#hamburger");
 const nav = document.querySelector("#navigation");
-
+const banner = document.querySelector(".current-date");
+const today = new Date()
+const day = today.getDate();
+// const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// const month = ["January","Febuary","March","April","May","June","July","August", "September","October","November","December"]
 
 menu.addEventListener("click", () => {
     nav.classList.toggle("responsive");
 })
+
+const banner_text = banner.appendChild(Object.assign(document.createElement("h3"), {
+    class: "banner-text",
+    innerHTML: "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
+}))
+
+if (day !== 1 | day !== 2) {
+    banner_text.style.display = "none";
+
+}
