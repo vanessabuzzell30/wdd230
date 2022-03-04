91bfe7908807da04b373f24a09f9c2195fb7d547
Vanessa Buzzell <buz20001@byui.edu>
2022-03-03 23:33:57 -0500

    c

---
+++ chamber/js/data.js
@@ -0,0 +1,88 @@
+{
+  "businesses" [
+    {
+      "name": "Dilly Deli",
+      "address": "Smith",
+      "phone": "23 December 1805",
+      "website": "27 June 1844",
+      "img-file": 14
+    },
+    {
+      "name": "Joseph",
+      "address": "Smith",
+      "phone": "23 December 1805",
+      "website": "27 June 1844",
+      "img-file": 14
+    },
+    {
+      "name": "Joseph",
+      "address": "Smith",
+      "phone": "23 December 1805",
+      "website": "27 June 1844",
+      "img-file": 14
+    },
+    {
+      "name": "Joseph",
+      "address": "Smith",
+      "phone": "23 December 1805",
+      "website": "27 June 1844",
+      "img-file": 14
+    },
+    {
+      "name": "Joseph",
+      "address": "Smith",
+      "phone": "23 December 1805",
+      "website": "27 June 1844",
+      "img-file": 14
+    },
+    {
+      "name": "Joseph",
+      "address": "Smith",
+      "phone": "23 December 1805",
+      "website": "27 June 1844",
+      "img-file": 14
+    },
+    {
+      "name": "Joseph",
+      "address": "Smith",
+      "phone": "23 December 1805",
+      "website": "27 June 1844",
+      "img-file": 14
+    },
+    {
+      "name": "Joseph",
+      "address": "Smith",
+      "phone": "23 December 1805",
+      "website": "27 June 1844",
+      "img-file": 14
+    },
+    {
+      "name": "Joseph",
+      "address": "Smith",
+      "phone": "23 December 1805",
+      "website": "27 June 1844",
+      "img-file": 14
+    },
+    {
+      "name": "Joseph",
+      "address": "Smith",
+      "phone": "23 December 1805",
+      "website": "27 June 1844",
+      "img-file": 14
+    },
+    {
+      "name": "Joseph",
+      "address": "Smith",
+      "phone": "23 December 1805",
+      "website": "27 June 1844",
+      "img-file": 14
+    },
+    {
+      "name": "Joseph",
+      "address": "Smith",
+      "phone": "23 December 1805",
+      "website": "27 June 1844",
+      "img-file": 14
+    }
+  ];
+};
---
+++ chamber/js/directory.js
@@ -0,0 +1,45 @@
+const requestURL = '';
+
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const businesses = jsonObject['businesses'];
+        // console.table(jsonObject);
+        businesses.forEach(displayBusinesses);
+    });
+
+function displayBusinesses(business) {
+  let card = document.createElement('section');
+
+  //img
+  let image = document.createElement('img');
+  image.src = business.imageurl;
+  image.setAttribute('alt', business.name);
+  card.appendChild(image);
+
+  //h2 name
+  let h2 = document.createElement('h2');
+  h2.textContent = business.name;
+  card.appendChild(h2);
+
+  //p address
+  let p1 = document.createElement('p');
+  p1.textContent = business.address;
+  card.appendChild(p1);
+
+  //p phone
+  let p2 = document.createElement('p');
+  p2.textContent = business.phone;
+  card.appendChild(p2);
+
+  //p website
+  let p3 = document.createElement('p');
+  p2.textContent = business.website;
+  card.appendChild(p3);
+
+  document.querySelector('div.cards').appendChild(card);
+
+}
