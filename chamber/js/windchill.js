14f4c2d9fb34cd61b53580c0375a5168b29bae8b
Vanessa Buzzell <buz20001@byui.edu>
2022-03-12 12:05:09 -0500

    fixes

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,10 +1,26 @@
-const t = parseInt(document.querySelector("#temp").textContent);
-const s = parseInt(document.getElementById("speed").textContent);
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Richmond&units=imperial&appid=8ce342ec0bef6453a2de8960f69f0afa'
 
-if(t <= 50 && s > 3) {
-    const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16)
-    document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}&#176;`;
-}
-else {
-    document.querySelector("#windchill").innerHTML = "N/A"
-}
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const t = jsObject.main.temp.toFixed(1);
+    document.querySelector('#current-temp').textContent = t;
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    const windsp = jsObject.wind.speed;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('#desc').textContent = desc;
+    document.querySelector('#speed').textContent = windsp;
+
+
+    if(t <= 50 && windsp > 3) {
+      const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(windsp,0.16) + 0.4275 * t * Math.pow(windsp,0.16)
+      document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}&#176;`;
+    }
+    else {
+        document.querySelector("#windchill").innerHTML = "N/A"
+    }
+  });
